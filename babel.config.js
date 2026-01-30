1d464dbcedff006f89a7645f20f700028507a944
girrajKD <girraj.khedmah@gmail.com>
2026-01-30 15:29:21 +0400

    setup: added required dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-worklets/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        allowUndefined: false,
+      },
+    ],
+  ],
 };
